7fee57b58b28a69e0203e87ec28738ef3b0af7de
sakibullah2006 <sakibullah.dev@gmail.com>
2026-08-16 04:46:33 +0600

    Refactor SliderTwo component for responsive design and improved image handling

--- components/Slider/SliderTwo.tsx
+++ components/Slider/SliderTwo.tsx
@@ -12,7 +12,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 const SliderTwo = () => {
     return (
         <>
-            <div className={`slider-block style-two bg-linear xl:h-[860px] lg:h-[800px] md:h-[580px] sm:h-[500px] h-[350px] max-[420px]:h-[400px] w-full`}>
+            <div className={`slider-block style-two bg-linear h-[calc(100dvh-30px)] md:h-[calc(100dvh-44px)] w-full`}>
                 <div className="slider-main h-full w-full">
                     <Swiper
                         spaceBetween={0}
@@ -34,13 +34,13 @@ const SliderTwo = () => {
                                         <div className="body1 text-center md:mt-4 mt-2">Check out our latest collection of chic and trendy outfits that will keep you looking stylish all year round.</div>
                                         <Link href={PATH.SHOP} className="button-main md:mt-8 mt-3">Shop Now</Link>
                                     </div>
-                                    <div className="sub-img absolute sm:w-1/2 w-[54%] 2xl:-right-[60px] right-0 bottom-0">
+                                    <div className="sub-img absolute sm:w-1/2 w-[54%] h-full 2xl:-right-[60px] right-0 bottom-0">
                                         <Image
                                             src={'/images/slider/bg1-1.webp'}
-                                            width={670}
-                                            height={936}
+                                            fill
                                             alt='bg2-1'
                                             priority={true}
+                                            className="object-contain object-bottom"
                                         />
                                     </div>
                                 </div>
@@ -55,13 +55,13 @@ const SliderTwo = () => {
                                         <div className="body1 text-center md:mt-4 mt-2">Get ready for the season with our latest collection of women{String.raw`'s`} clothing</div>
                                         <Link href={PATH.SHOP} className="button-main md:mt-8 mt-3">Shop Now</Link>
                                     </div>
-                                    <div className="sub-img absolute sm:w-1/2 w-3/5 2xl:-right-[60px] -right-4 bottom-0">
+                                    <div className="sub-img absolute sm:w-1/2 w-3/5 h-full 2xl:-right-[60px] -right-4 bottom-0">
                                         <Image
                                             src={'/images/slider/bg1-2.webp'}
-                                            width={670}
-                                            height={936}
+                                            fill
                                             alt='bg2-2'
                                             priority={true}
+                                            className="object-contain object-bottom"
                                         />
                                     </div>
                                 </div>
@@ -76,14 +76,13 @@ const SliderTwo = () => {
                                         <div className="body1 text-center md:mt-4 mt-2">Check out our curated selection of must-have outfits for any occasion</div>
                                         <Link href={PATH.SHOP} className="button-main md:mt-8 mt-3">Shop Now</Link>
                                     </div>
-                                    <div className="sub-img basis-1/2 h-full flex items-end">
+                                    <div className="sub-img basis-1/2 h-full relative">
                                         <Image
                                             src={'/images/slider/bg1-3.webp'}
-                                            width={670}
-                                            height={936}
+                                            fill
                                             alt='bg2-3'
                                             priority={true}
-                                            className="w-full h-auto object-contain"
+                                            className="object-contain object-bottom"
                                         />
                                     </div>
                                 </div>
